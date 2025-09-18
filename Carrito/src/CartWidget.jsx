8c871df31c616535e32696c2cc520c63fabbabb7
Armine Garcia <armineg@hotmail.com>
2025-09-18 17:13:55 -0600

    conteo del carrito agregado

--- Carrito/src/CartWidget.jsx
+++ Carrito/src/CartWidget.jsx
@@ -1,8 +1,11 @@
-export const Carrito = () => (
-    <img className ="imagenBarra" src="./src/assets/carrito.svg" alt="tu carrito de compras" />
+let conteo = 1
 
+
+export const Carrito = () => (
+
+    <div className="areaCarrito" >
+        <img className = "imagenBarra" src="./src/assets/carrito.svg" alt="tu carrito de compras" />
+        <span className="conteo">{conteo}</span>
+    </div>
 
 )
-
-
-
